757ed429125e0ec76a161de70377a2f9b3b8cc4d
Jake Butler <tjakebutler@gmail.com>
2021-03-01 16:19:49 -0600

    Made my way through #8

---
+++ 6sales_agent_invoices.sql
@@ -0,0 +1,8 @@
+Provide a query that shows the invoices associated with each sales agent. The resultant table should include the Sales Agents full name.
+
+SELECT Invoice.InvoiceId,
+Employee.FirstName,
+Employee.LastName
+FROM Invoice 
+JOIN Customer ON Invoice.CustomerId = Customer.CustomerId
+JOIN Employee ON Customer.SupportRepId = Employee.EmployeeId
